6f5a08aa7a4e93229e7a42a4a9801943f2b3c131
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-02 00:12:22 +0200

    new file:   functions-deep/functionvalues/functionvalues.go 	modified:   functions-deep/main.go

--- functions-deep/main.go
+++ functions-deep/main.go
@@ -1,53 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
-
-type transformFn func(int) int
+import "fmt"
 
 func main() {
-	numbers := []int{1, 2, 3, 4}
-	moreNumbers := []int{5, 1, 2}
-	doubled := transformNumbers(&numbers, double)
-	tripled := transformNumbers(&numbers, triple)
+	numbers := []int{1, 2, 3}
 
-	fmt.Println(doubled)
-	fmt.Println(tripled)
+	transformed := transformNumbers(&numbers, func(number int) int {
+		return number * 2
+	})
 
-	transformFn1 := getTransformemFunction(&numbers)
-	transformFn2 := getTransformemFunction(&moreNumbers)
-
-	transformedNumbers := transformNumbers(&numbers, transformFn1)
-	transformedNumbers2 := transformNumbers(&moreNumbers, transformFn2)
-
-	fmt.Printf("Transformacja zwykla: %v\nTransformacja wieksza: %v\n", transformedNumbers, transformedNumbers2)
+	fmt.Println(transformed)
 }
 
-// bardzo przydatne bo wprowadzamy zmienna ktora przechowuja wybrana przez nas funkcje
-func transformNumbers(numbers *[]int, transform transformFn) []int {
-
+func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	dNumbers := []int{}
+
 	for _, val := range *numbers {
 		dNumbers = append(dNumbers, transform(val))
 	}
+
 	return dNumbers
-}
-
-func getTransformemFunction(numbers *[]int) transformFn {
-	if (*numbers)[0] == 1 {
-		return double
-	} else {
-		return triple
-	}
-}
-
-func double(number int) int {
-	return number * 2
-
-}
-
-func triple(number int) int {
-	return number * 3
-
 }
