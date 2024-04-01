7593858a7152b9116965cac2f3677a9a61448277
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-01 23:16:03 +0200

    modified:   functions-deep/main.go

--- functions-deep/main.go
+++ functions-deep/main.go
@@ -1,16 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type transformFn func(int) int
 
 func main() {
 	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
 	doubled := transformNumbers(&numbers, double)
 	tripled := transformNumbers(&numbers, triple)
 
 	fmt.Println(doubled)
 	fmt.Println(tripled)
+
+	transformFn1 := getTransformemFunction(&numbers)
+	transformFn2 := getTransformemFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	transformedNumbers2 := transformNumbers(&moreNumbers, transformFn2)
+
+	fmt.Printf("Transformacja zwykla: %v\nTransformacja wieksza: %v\n", transformedNumbers, transformedNumbers2)
 }
 
 // bardzo przydatne bo wprowadzamy zmienna ktora przechowuja wybrana przez nas funkcje
@@ -21,6 +32,14 @@ func transformNumbers(numbers *[]int, transform transformFn) []int {
 		dNumbers = append(dNumbers, transform(val))
 	}
 	return dNumbers
+}
+
+func getTransformemFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
 }
 
 func double(number int) int {
