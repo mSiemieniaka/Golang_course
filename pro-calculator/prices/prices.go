d20af382e35dc7bafda1f128f0371b29d917c3e7
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-07 20:44:07 +0200

    modified:   pro-calculator/prices/prices.go

--- pro-calculator/prices/prices.go
+++ pro-calculator/prices/prices.go
@@ -1,11 +1,59 @@
 package prices
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 type TaxIncludedPriceJob struct {
 	TaxRate           float64
 	InputPrices       []float64
 	TaxIncludedPrices map[string]float64
+}
+
+func (job TaxIncludedPriceJob) LoadData() {
+	file, err := os.Open("prices.txt")
+
+	if err != nil {
+		fmt.Println("Error 1")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	scanner := bufio.NewScanner(file)
+
+	lines := []string{}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+
+	if err != nil {
+		fmt.Println("Error 2")
+		fmt.Println(err)
+		file.Close()
+		return
+	}
+
+	prices := make([]float64, len(lines))
+
+	for lineIndex, line := range lines {
+		floatPrice, err := strconv.ParseFloat(line, 64)
+
+		if err != nil {
+			fmt.Println("Error 3")
+			fmt.Println(err)
+			file.Close()
+			return
+		}
+		prices[lineIndex] = floatPrice
+	}
+
+	job.InputPrices = prices
 }
 
 func (job TaxIncludedPriceJob) Process() {
