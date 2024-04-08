fe84408a2ce669a08c746339a523964ae836e0f4
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-08 23:32:20 +0200

    multiple channels as a slice

--- concurenncy/main.go
+++ concurenncy/main.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-func greet(pharse string) {
+func greet(pharse string, doneChan chan bool) {
 	fmt.Println("Hello!", pharse)
+	doneChan <- true
 }
 
 func slowGreet(pharse string, doneChan chan bool) {
@@ -16,9 +17,16 @@ func slowGreet(pharse string, doneChan chan bool) {
 }
 
 func main() {
-	//go greet("Nice to meet you")
-	//go greet("How are you")
-	done := make(chan bool)
-	go slowGreet("How ... are ... you ...?", done)
-	<- done
+	dones := make([]chan bool, 3)
+	//done := make(chan bool)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you", dones[1])
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", dones[2])
+
+	for _, done := range dones {
+		<- done
+	}
 }
