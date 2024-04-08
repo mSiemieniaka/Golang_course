918d0049c419cc567432230a10e8d1a125fe1ae3
m.siemieniaka <marcin.siemieniaka@gmail.com>
2024-04-08 23:25:47 +0200

    channel one for Go

--- concurenncy/main.go
+++ concurenncy/main.go
@@ -9,13 +9,16 @@ func greet(pharse string) {
 	fmt.Println("Hello!", pharse)
 }
 
-func slowGreet(pharse string) {
+func slowGreet(pharse string, doneChan chan bool) {
 	time.Sleep(3 * time.Second)
 	fmt.Println("Hello! po 3 sekundach", pharse)
+	doneChan <- true
 }
 
 func main() {
-	go greet("Nice to meet you")
-	go greet("How are you")
-	go slowGreet("How ... are ... you ...?")
+	//go greet("Nice to meet you")
+	//go greet("How are you")
+	done := make(chan bool)
+	go slowGreet("How ... are ... you ...?", done)
+	<- done
 }
